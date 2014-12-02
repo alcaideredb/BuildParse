a345f37bb705070109705a15b34305da9d9fdf74
alcaideredb <alcaideredb@gmail.com>
2014-12-03 02:36:08 +0800

    added journal

--- moc_journal.cpp
+++ moc_journal.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_Journal_t {
-    QByteArrayData data[6];
-    char stringdata[82];
+    QByteArrayData data[7];
+    char stringdata[105];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -34,11 +34,12 @@ QT_MOC_LITERAL(1, 8, 21),
 QT_MOC_LITERAL(2, 30, 0),
 QT_MOC_LITERAL(3, 31, 23),
 QT_MOC_LITERAL(4, 55, 20),
-QT_MOC_LITERAL(5, 76, 5)
+QT_MOC_LITERAL(5, 76, 5),
+QT_MOC_LITERAL(6, 82, 22)
     },
     "Journal\0on_toolButton_clicked\0\0"
     "on_toolButton_3_clicked\0on_tableView_clicked\0"
-    "index"
+    "index\0on_tableView_activated"
 };
 #undef QT_MOC_LITERAL
 
@@ -48,7 +49,7 @@ static const uint qt_meta_data_Journal[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -56,13 +57,15 @@ static const uint qt_meta_data_Journal[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   29,    2, 0x08 /* Private */,
-       3,    0,   30,    2, 0x08 /* Private */,
-       4,    1,   31,    2, 0x08 /* Private */,
+       1,    0,   34,    2, 0x08 /* Private */,
+       3,    0,   35,    2, 0x08 /* Private */,
+       4,    1,   36,    2, 0x08 /* Private */,
+       6,    1,   39,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void, QMetaType::QModelIndex,    5,
     QMetaType::Void, QMetaType::QModelIndex,    5,
 
        0        // eod
@@ -76,6 +79,7 @@ void Journal::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, voi
         case 0: _t->on_toolButton_clicked(); break;
         case 1: _t->on_toolButton_3_clicked(); break;
         case 2: _t->on_tableView_clicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 3: _t->on_tableView_activated((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
         default: ;
         }
     }
@@ -106,13 +110,13 @@ int Journal::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 4)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 3;
+        _id -= 4;
     }
     return _id;
 }
--- ui_journal.h
+++ ui_journal.h
@@ -15,8 +15,10 @@
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QDialog>
 #include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLineEdit>
+#include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QTableView>
 #include <QtWidgets/QToolButton>
 #include <QtWidgets/QVBoxLayout>
@@ -28,11 +30,15 @@ class Ui_Journal
 public:
     QGridLayout *gridLayout;
     QToolButton *toolButton;
-    QToolButton *toolButton_3;
-    QToolButton *toolButton_2;
     QLineEdit *lineEdit;
     QVBoxLayout *verticalLayout;
     QTableView *tableView;
+    QGridLayout *gridLayout_2;
+    QSpacerItem *verticalSpacer;
+    QTableView *tableView_2;
+    QHBoxLayout *horizontalLayout_3;
+    QToolButton *toolButton_2;
+    QToolButton *toolButton_3;
 
     void setupUi(QDialog *Journal)
     {
@@ -45,16 +51,6 @@ public:
         toolButton->setObjectName(QStringLiteral("toolButton"));
 
         gridLayout->addWidget(toolButton, 0, 0, 1, 1);
-
-        toolButton_3 = new QToolButton(Journal);
-        toolButton_3->setObjectName(QStringLiteral("toolButton_3"));
-
-        gridLayout->addWidget(toolButton_3, 0, 3, 1, 1);
-
-        toolButton_2 = new QToolButton(Journal);
-        toolButton_2->setObjectName(QStringLiteral("toolButton_2"));
-
-        gridLayout->addWidget(toolButton_2, 0, 2, 1, 1);
 
         lineEdit = new QLineEdit(Journal);
         lineEdit->setObjectName(QStringLiteral("lineEdit"));
@@ -71,6 +67,36 @@ public:
 
         gridLayout->addLayout(verticalLayout, 1, 1, 1, 1);
 
+        gridLayout_2 = new QGridLayout();
+        gridLayout_2->setObjectName(QStringLiteral("gridLayout_2"));
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        gridLayout_2->addItem(verticalSpacer, 1, 0, 1, 1);
+
+        tableView_2 = new QTableView(Journal);
+        tableView_2->setObjectName(QStringLiteral("tableView_2"));
+        tableView_2->setMaximumSize(QSize(150, 300));
+
+        gridLayout_2->addWidget(tableView_2, 0, 0, 1, 1);
+
+
+        gridLayout->addLayout(gridLayout_2, 1, 2, 1, 1);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
+        toolButton_2 = new QToolButton(Journal);
+        toolButton_2->setObjectName(QStringLiteral("toolButton_2"));
+
+        horizontalLayout_3->addWidget(toolButton_2);
+
+        toolButton_3 = new QToolButton(Journal);
+        toolButton_3->setObjectName(QStringLiteral("toolButton_3"));
+
+        horizontalLayout_3->addWidget(toolButton_3);
+
+
+        gridLayout->addLayout(horizontalLayout_3, 0, 2, 1, 1);
+
 
         retranslateUi(Journal);
 
@@ -81,8 +107,8 @@ public:
     {
         Journal->setWindowTitle(QApplication::translate("Journal", "Dialog", 0));
         toolButton->setText(QApplication::translate("Journal", "Add Journal", 0));
-        toolButton_3->setText(QApplication::translate("Journal", "Delete Journal", 0));
         toolButton_2->setText(QApplication::translate("Journal", "Edit Journal", 0));
+        toolButton_3->setText(QApplication::translate("Journal", "Delete Journal", 0));
     } // retranslateUi
 
 };
